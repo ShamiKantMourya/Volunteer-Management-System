0568ec79d419d5d4a1a76f3c09acc2b65aa73ee1
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-16 14:58:17 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import StorageIcon from '@mui/icons-material/Storage';
+export default function Header(){
+  return <>
+    <div className="logo">Events</div>
+    <nav>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/volunteer'>Volunteers</NavLink>
+      <NavLink to='/events'>Events</NavLink>
+      <NavLink to='https://github.com/Anush79/VolunteerManagement' target="_blank"><GitHubIcon/></NavLink>
+      <NavLink to='https://replit.com/@shamiMourya/Volunteer-Management-APIs#index.js'target="_blank"><StorageIcon/></NavLink>
+
+    </nav>
+  </>
+}
