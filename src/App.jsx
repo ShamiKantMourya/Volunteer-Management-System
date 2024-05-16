64a782fcc4cd964e1f3ba29cea35cbd49e0c9654
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-16 17:30:32 +0530

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 
-import './App.css'
+import Dashboard from "./features/Dashboard";
+import Wards from "./features/events/Events";
+import Header from "./components/Header";
+import { fetchEvents } from "./features/events/eventSlice";
+import ShowDetails from "./components/ShowDetails";
+import HotToast from "./components/HotToast";
+import Volunteer from "./features/volunteer/Volunteer";
+import { fetchVolunteers } from "./features/volunteer/volunteerSlice";
+import Events from "./features/events/Events";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const { status:vloading } = useSelector((state) => state?.volunteers
+  );
+  const { status: eLoading } = useSelector((state) => state?.events);
+
+  useEffect(() => {
+    dispatch(fetchVolunteers());
+    dispatch(fetchEvents());
+  }, [dispatch]);
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+
+      <header>
+        <Header />
+      </header>
+      <HotToast />
+      <main>
+        {vloading ==='loading' || eLoading==='loading' ? <div className="loader"></div> : null}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/volunteer" element={<Volunteer />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/:type/:id" element={<ShowDetails />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import "./index.css";
+import store from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>
+);
